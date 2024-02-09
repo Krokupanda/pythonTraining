34976bd2c919e1711f296cb5d5b5677ab61dc18a
Alex Gulyaev <Shareaza79@yandex.ru>
2024-02-09 13:32:58 +0400

    complicated task beautiful number

--- training_tests.py
+++ training_tests.py
@@ -1,17 +1,5 @@
-a = int(input())
-b = int(input())
-c = int(input())
-
-if 0 < a:
-    pos_a = a
+x = int(input())
+if (0 < x // 1000 < 10) and (x % 7 == 0 or x % 17 == 0 ):
+    print('YES')
 else:
-    pos_a = 0    
-if 0 < b:
-    pos_b = b
-else:
-    pos_b = 0
-if 0 < c:
-    pos_c = c
-else:
-    pos_c = 0
-print(pos_a + pos_b + pos_c)
+    print('NO')
