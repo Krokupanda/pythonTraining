d5809a0bc44ed8b48c15ae3ccb899514834e26ee
Alex Gulyaev <Shareaza79@yandex.ru>
2024-02-12 16:10:48 +0400

    roulette color checking

--- training_tests.py
+++ training_tests.py
@@ -1,5 +1,11 @@
-x = int(input())
-if (0 < x // 1000 < 10) and (x % 7 == 0 or x % 17 == 0 ):
-    print('YES')
+color = int(input())
+if 0 <= color <= 36:
+    if ((0 < color <= 10) and (color % 2 == 0)) or ((11 <= color <= 18) and (color % 2 != 0) or ((19 <= color <= 28) and (color % 2 == 0)) or ((29 <= color <= 36) and (color % 2 != 0))):
+        print('черный')
+    elif ((0 < color <= 10) and (color % 2 != 0)) or ((11 <= color <= 18) and (color % 2 == 0) or ((19 <= color <= 28) and (color % 2 != 0)) or ((29 <= color <= 36) and (color % 2 == 0))):
+        print('красный')
+    else:
+        print('зеленый')
+
 else:
-    print('NO')
+    print('ошибка ввода')
